d9855fcc8066f7c545eb656d9de4d24f372d408b
ry <yamaguchiryoma@yamaguchiryoushinnoMacBook-Pro.local>
2020-03-07 11:27:39 +0900

    learnstate

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
 import React, { Component } from 'react';
-const App = () => {
-  const profiles = [
-    {
-      name: 'taro',
-      age: 10
-    },
-    {
-      name: 'hanako',
-      age: 5
-    },
-    {
-      name: 'hanako'
+
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component {
+  constructor(props) {//カウンタークラスのインスタンスが作成された際に、コンストラクターのメソッドが呼び出される。（初期化する）
+    super(props)//
+    this.state = { count: 0 }//初期化した後に、オブジェクトが作られる。今回はcounteが作られた。
+  }
+  handlePlusButton = () => {
+    const CurrentCount = this.state.count;
+    this.setState({ count: this.state.count + 1 })//setStateが実行されると、renderが再度描画される。
+  }
+  handleMinusButton = () => {
+    const CurrentCount = this.state.count;
+    this.setState({ count: this.state.count - 1 })
+    if (this.state.count % 2 == 0) {
     }
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-    </div>
-  )
-}
-
-const User = (props) => {
-  return <div>Hi im {props.name}, and I'm {props.age}years old!</div>
-}
-
-User.defaultProps = {
-  age: '--'
+  }
+  render() {
+    console.log(this.state);
+    return (
+      <React.Fragment>
+        <div>count:{this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
