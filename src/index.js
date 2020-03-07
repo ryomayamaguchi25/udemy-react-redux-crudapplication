d2b38d97305bd6384c2f27fc9c81fa5f9948faef
ry <yamaguchiryoma@yamaguchiryoushinnoMacBook-Pro.local>
2020-03-07 14:09:01 +0900

    fix path

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';//どのコンポーネントからも参照できるようにする
 
 import './index.css';
 import reducer from './reducers'
 
-import App from '../components/App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
+
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
